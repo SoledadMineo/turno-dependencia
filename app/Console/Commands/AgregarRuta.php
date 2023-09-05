0c3ce15821bf84565b4bfe3b61c81d9e8bed45b0
Fernando Muñoz <biblioclasta@gmail.com>
2023-09-05 16:45:09 -0300

    ruta segura

--- app/Console/Commands/AgregarRuta.php
+++ app/Console/Commands/AgregarRuta.php
@@ -35,6 +35,28 @@ class AgregarRuta extends Command
                 "type" => "roundrobin"
             ]
             ]);
+            $resp = Http::withHeaders(['X-Api-Key'=> 'edd1c9f034335f136f87ad84b625c8f1'])
+            ->post('http://apisix:9180/apisix/admin/routes', [
+            "uri" => "/hola/*",
+            "name"=> "hola_seguro",
+            "upstream"=> [
+                "nodes"=> ["nginx-hola_mundo:80"=>1],
+                "type" => "roundrobin"
+            ],
+            "plugins"=> [
+                "openid-connect" => [
+                    "client_id" =>"mpf",
+                    "client_secret" =>"d5c42c50-3e71-4bbe-aa9e-31083ab29da4",
+                    "discovery" =>"http://127.0.0.1:7082/auth/realms/master/.well-known/openid-configuration",
+                    "scope" => "openid profile",
+                    "bearer_only" => false,
+                    "realm" =>"master",
+                    //"introspection_endpoint_auth_method" =>"client_secret_post",
+                    "redirect_uri" => "http://127.0.0.1:7080/"
+                ]
+            ],
+
+        ]);
         return $resp->json();
 
     }
